0310f10a0b6dd6b7746e5d53cc2d9ad5fceb58c9
ivponicka <56002673+ivponicka@users.noreply.github.com>
2024-03-30 22:28:00 +0100

    added categories / add & delete options

--- jewex/src/main/java/com/example/jewex/controller/UserController.java
+++ jewex/src/main/java/com/example/jewex/controller/UserController.java
@@ -1,14 +1,65 @@
 package com.example.jewex.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.jewex.model.Category;
+import com.example.jewex.service.CategoryService;
+
+import org.springframework.web.bind.annotation.PostMapping;
+
+
 
 
 @Controller
 public class UserController {
+
+    @Autowired
+     CategoryService categoryService;
    
     @GetMapping("/shop")
     public String shopping(){
         return "shop";
+    }
+
+      @GetMapping("/admin/products")
+    public String products(){
+        return "admin_products";
+    }
+
+    @GetMapping("/admin")
+    public String adminHome(){
+        return "admin_home";
+    }
+
+     @GetMapping("/admin/categories")
+    public String categories(Model model){
+        model.addAttribute("categories", categoryService.getAllCategories());
+        return "admin_categories";
+    }
+
+
+    @GetMapping("/admin/categories/addCategory")
+    public String getAddCategory(Model model){
+        model.addAttribute("category", new Category());
+        return "admin_add_category";
+    }
+
+    @PostMapping("/admin/categories/addCategory")
+    public String postAddCategory(@ModelAttribute("category") Category category) {
+        categoryService.addCategory(category);
+        return "redirect:/admin/categories";
+    }
+    
+     @GetMapping("/admin/categories/deleteCategory/{id}")
+    public String deleteCategory(@PathVariable int id){
+         categoryService.deleteCategory(id);
+        return "redirect:/admin/categories";
     }
 }
---
+++ jewex/src/main/java/com/example/jewex/service/CategoryService.java
@@ -0,0 +1,33 @@
+package com.example.jewex.service;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.jewex.model.Category;
+import com.example.jewex.repository.CategoryRepository;
+
+@Service
+public class CategoryService {
+    @Autowired
+    CategoryRepository categoryRepository;
+
+     public List<Category> getAllCategories(){
+        return categoryRepository.findAll();
+    }
+
+    public void addCategory(Category category){
+        categoryRepository.save(category);
+    }
+
+    public Optional<Category> findCategory(Integer id){
+        return categoryRepository.findById(id);
+    }
+
+    public void deleteCategory(int id){
+        categoryRepository.deleteById(id);
+    }
+}
